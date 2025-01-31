7ead8950bb2ea623f078060fe77f3a494a230cb3
visam <visamvenkatasai9550@gmail.com>
2025-01-31 11:25:47 +0530

    Updated data files

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import {Provider} from "react-redux"
-import Store from './components/Redux/Store'
+import Store from './components/Redux/store'
 
 createRoot(document.getElementById('root')).render(
   <Provider store = {Store}>
